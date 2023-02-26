6bc3cbe53d238d9e19fec469e8cb1db6b46b05b0
MRegterschot <marijn.regterschot@gmail.com>
2023-02-26 16:54:08 +0100

    feat: added navbar, main and footer

---
+++ components/footer/style.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0F0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default styles;
